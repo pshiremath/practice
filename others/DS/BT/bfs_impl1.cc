6587b6b7be62ea907ad59af57fe1cde49dcbfada
Prashant Hiremath <prashanthiremath583@gmail.com>
2016-03-25 13:48:54 +0530

    Breadth First Traversal without Queue.

---
+++ others/DS/BT/bfs_impl1.cc
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+typedef struct _TREE {
+  int data;
+  _TREE* left;
+  _TREE* right;
+} TREE;
+
+TREE* newNode(int data) {
+  TREE* node = new TREE();
+  node->data = data;
+  node->left = nullptr;
+  node->right = nullptr;
+
+  return node;
+}
+
+int heightOfTree(TREE* node) {
+  if (node == nullptr) {
+    return 0;
+  } else {
+    int lheight = heightOfTree(node->left);
+    int rheight = heightOfTree(node->right);
+
+    if (lheight > rheight) {
+      return (lheight + 1);
+    } else {
+      return (rheight + 1);
+    }
+  }
+}
+
+void printGivenLevel(TREE* root, int level) {
+  if (root == nullptr) return;
+  if (level == 1)
+    printf("%d ", root->data);
+  else if (level > 1) {
+    printGivenLevel(root->left, level - 1);
+    printGivenLevel(root->right, level - 1);
+  }
+}
+
+void printLevelOrder(TREE* node) {
+  int height = heightOfTree(node);
+  for (int i = 0; i <= height; i++) {
+    printGivenLevel(node, i);
+  }
+}
+
+int main(int argc, char const* argv[]) {
+  TREE* root = newNode(1);
+  root->left = newNode(2);
+  root->right = newNode(3);
+  root->left->left = newNode(4);
+  root->left->right = newNode(5);
+
+  printf("Level Order traversal of binary tree is \n");
+  printLevelOrder(root);
+  return 0;
+}
